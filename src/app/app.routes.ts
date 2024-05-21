2d34b14935aa915d82cde48d3b300b04b0666f5c
Miguel Lopez <MiguelLopez@email.com>
2024-05-21 16:01:38 +0200

    MiguelCreadoYRutaPuesta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { MiguelComponent } from './components/miguel/miguel.component';
 
 
 export const routes: Routes = [
     { path: '',   redirectTo: '/home', pathMatch: 'full' },
     {path: 'home', component: HomeComponent},
     {path: 'navbar', component: NavbarComponent},
+    {path: 'miguel', component: MiguelComponent},
     //las rutas deben agregarse a partir de aqui
 
 
