7d9b9c65f0328265ed28e9f1a0dae1311450ba7a
walysson <walysson.correaaraujo@cieep.com>
2024-05-21 15:59:09 +0200

    componente walysson agregado, ruta creada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { WalyssonComponent } from './components/walysson/walysson.component';
 
 
 export const routes: Routes = [
     { path: '',   redirectTo: '/home', pathMatch: 'full' },
     {path: 'home', component: HomeComponent},
     {path: 'navbar', component: NavbarComponent},
+    {path: 'walysson', component: WalyssonComponent},
     //las rutas deben agregarse a partir de aqui
 
 
